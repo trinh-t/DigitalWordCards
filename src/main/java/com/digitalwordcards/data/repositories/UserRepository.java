62ddf821cd41c86e73e48ed746977038276f379e
Tim Trinh <tim@brandgeeks.nl>
2022-02-27 08:43:10 +0100

    Fout in repository code, aangepast.

--- src/main/java/com/digitalwordcards/data/repositories/UserRepository.java
+++ src/main/java/com/digitalwordcards/data/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package com.digitalwordcards.data.repositories;
 
+import com.digitalwordcards.data.Card;
+import com.digitalwordcards.data.Role;
 import com.digitalwordcards.data.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +11,10 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    List<User> findByClazz(String clazz);
+
+    List<User> findByClazzAndAndRole(String clazz, Role role);
     Optional<User> findUserByEmail(String email);
+
+    List<User> findAllByViewedCardsIsContaining(Card card);
+
 }
